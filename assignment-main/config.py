a8271b6c3b7aeceeaa9de5ebc696a473031752d6
wky0710 <89335841+wky0710@users.noreply.github.com>
2023-09-19 20:13:25 +0800

    initial commit

--- assignment-main/config.py
+++ assignment-main/config.py
@@ -1,7 +1,7 @@
 customhost = "database-2.cyltbqip9evc.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "abcd1234"
-customdb = "itps"
-custombucket = "ongjingrou-itps"
+custompass = "Bait3273"
+customdb = "student"
+custombucket = "wongkaryan-student"
 customregion = "us-east-1"
 
