ee43ec597f6691e2c2de2f45e6cd577c27cce643
wky0710 <89335841+wky0710@users.noreply.github.com>
2023-09-20 01:05:10 +0800

    initial commit

--- assignment-main/app.py
+++ assignment-main/app.py
@@ -138,38 +138,37 @@ def jobReg():
         insert_sql = "INSERT INTO jobApply VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
         cursor = db_conn.cursor()
 
-        # if companyImage.filename == "":
-        #     return "Please select a file"
+        if companyImage.filename == "":
+            return "Please select a file"
  
         cursor.execute(insert_sql, (comp_name, job_title, job_desc, job_req, sal_range, contact_person_name, contact_person_email, contact_number, comp_state))
         db_conn.commit()
         cursor.close()
 
-        return redirect(url_for('companyDashboard'))
-        # # Uplaod image file in S3 #
-        # comp_image_file_name_in_s3 = "company-" + str(comp_name) + "_image_file"
-        # s3 = boto3.resource('s3')
+        # Uplaod image file in S3 #
+        comp_image_file_name_in_s3 = "company-" + str(comp_name) + "_image_file"
+        s3 = boto3.resource('s3')
         
-        # try:
-        #     print("Data inserted in MySQL RDS... uploading image to S3...")
-        #     s3.Bucket(custombucket).put_object(Key=comp_image_file_name_in_s3, Body=companyImage)
-        #     bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-        #     s3_location = (bucket_location['LocationConstraint'])
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=comp_image_file_name_in_s3, Body=companyImage)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
 
-        #     if s3_location is None:
-        #         s3_location = ''
-        #     else:
-        #         s3_location = '-' + s3_location
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
 
-        #     object_url = "https://s3%7B0%7D.amazonaws.com/%7B1%7D/%7B2%7D".format(
-        #         s3_location,
-        #         custombucket,
-        #         comp_image_file_name_in_s3)
-        #     return redirect(url_for('companyDashboard'))
-        # except Exception as e:
-        #     cursor.close()
-        #     print(f"Error during database insertion: {e}")
-        #     return str(e)  # Handle any database errors here
+            object_url = "https://s3%7B0%7D.amazonaws.com/%7B1%7D/%7B2%7D".format(
+                s3_location,
+                custombucket,
+                comp_image_file_name_in_s3)
+            return redirect(url_for('companyDashboard'))
+        except Exception as e:
+            cursor.close()
+            print(f"Error during database insertion: {e}")
+            return str(e)  # Handle any database errors here
 
     return render_template('jobReg.html')
 
