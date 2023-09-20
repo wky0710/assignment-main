f2360786e99829be13dd246d813413ab2a77bb32
wky0710 <89335841+wky0710@users.noreply.github.com>
2023-09-21 00:02:30 +0800

    commit

--- assignment-main/app.py
+++ assignment-main/app.py
@@ -3,6 +3,7 @@ from pymysql import connections
 import os
 import boto3
 from config import *
+import hashlib
 
 app = Flask(__name__)
 # Configure the 'templates' folder for HTML templates.
@@ -21,39 +22,48 @@ db_conn = connections.Connection(
 
 )
 output = {}
-table = 'jobApply'
+table = 'students'
+
 
 @app.route("/", methods=['GET'], endpoint='index')
 def index():
     return render_template('index.html')
 
+
 @app.route("/job_listing", methods=['GET'])
 def job_listing():
     return render_template('job_listing.html')
+
 
 @app.route("/about", methods=['GET'])
 def about():
     return render_template('about.html')
 
+
 @app.route("/blog", methods=['GET'])
 def blog():
     return render_template('blog.html')
+
 
 @app.route("/single_blog", methods=['GET'])
 def single_blog():
     return render_template('single_blog.html')
 
+
 @app.route("/elements", methods=['GET'])
 def elements():
     return render_template('elements.html')
+
 
 @app.route("/job_details", methods=['GET'])
 def job_details():
     return render_template('job_details.html')
 
+
 @app.route("/contact", methods=['GET'])
 def contact():
     return render_template('contact.html')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -70,16 +80,47 @@ def register():
         cgpa = request.form['cgpa']
         ucSupervisor = request.form['ucSupervisor']
         ucSupervisorEmail = request.form['ucSupervisorEmail']
-        
+
+        # Check if the email is already in the database.
+        cursor = db_conn.cursor()
+        cursor.execute("SELECT * FROM students WHERE stud_email=%s", (email))
+        results = cursor.fetchall()
+        cursor.close()
+
+        # If the email is already in the database, return an error message to the user and display it on the register.html page.
+        if len(results) > 0:
+            return render_template('register.html', email_error="The email is already in use.")
+
+        # Otherwise, check if the IC is already in the database.
+        cursor = db_conn.cursor()
+        cursor.execute("SELECT * FROM students WHERE ic=%s", (ic))
+        results = cursor.fetchall()
+        cursor.close()
+
+        # If the IC is already in the database, return an error message to the user and display it on the register.html page.
+        if len(results) > 0:
+            return render_template('register.html', ic_error="The IC is already in use.")
+
+        # Otherwise, check if the student ID is already in the database.
+        cursor = db_conn.cursor()
+        cursor.execute(
+            "SELECT * FROM students WHERE studentID=%s", (studentID))
+        results = cursor.fetchall()
+        cursor.close()
+
+        # If the student ID is already in the database, return an error message to the user and display it on the register.html page.
+        if len(results) > 0:
+            return render_template('register.html', studentID_error="The student ID is already in use.")
+
         insert_sql = "INSERT INTO students VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         cursor = db_conn.cursor()
-        
+
         try:
-            cursor.execute(insert_sql, (email, 
-                                        studentID, 
-                                        firstName, 
-                                        lastName, 
+            cursor.execute(insert_sql, (studentID,
+                                        firstName,
+                                        lastName,
                                         gender,
+                                        email,
                                         password,
                                         ic,
                                         programmeSelect,
@@ -89,38 +130,218 @@ def register():
                                         ))
             db_conn.commit()
             cursor.close()
-            return redirect(url_for('login'))  # Redirect to the homepage after successful registration
+            # Redirect to the homepage after successful registration
+            return redirect(url_for('login'))
         except Exception as e:
             cursor.close()
             return str(e)  # Handle any database errors here
-    
+
     return render_template('register.html')
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return render_template('index.html', user_authenticated=True)
-    
-    # Fetch data from the database here
-    cursor = db_conn.cursor()
-    select_sql = "SELECT email, password FROM students"
-    cursor.execute(select_sql)
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('login.html', students=data)
+        email = request.form.get('email')
+        password = request.form.get('password')
+        role = request.form.get('role')
+
+        if role == 'Student':
+            # Fetch data from the database here
+            cursor = db_conn.cursor()
+            select_sql = "SELECT stud_email, password, firstName, studentID FROM students WHERE stud_email = %s"
+            cursor.execute(select_sql, (email,))
+            data = cursor.fetchone()  # Fetch a single row
+
+            if data:
+                # Data is found in the database
+                stored_password = data[1]
+                name = data[2]
+                studID = data[3]
+
+                # You should hash the provided password and compare it to the stored hashed password
+                hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+                if password == stored_password:
+                    # Passwords match, user is authenticated
+                    return render_template('index.html', user_login_name=name, studentID=studID, user_authenticated=True)
+                else:
+                    return render_template('login.html', pwd_error="Incorrect password. Please try again.")
+            else:
+                return render_template('login.html', email_login_error="Email not found. Please register or try a different email.")
+        elif role == 'Company':
+             # Fetch data from the database here
+            cursor = db_conn.cursor()
+            select_sql = "SELECT compEmail, comPassword, compName FROM company WHERE compEmail = %s"
+            cursor.execute(select_sql, (email,))
+            data = cursor.fetchone()  # Fetch a single row
+
+            if data:
+                # Data is found in the database
+                stored_password = data[1]
+                name = data[2]
+
+                # You should hash the provided password and compare it to the stored hashed password
+                hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+                if password == stored_password:
+                    # Passwords match, user is authenticated
+                    return render_template('companyDashboard.html')
+                else:
+                    return render_template('login.html', pwd_error="Incorrect password. Please try again.")
+            else:
+                return render_template('login.html', email_login_error="Email not found. Please register or try a different email.")
+            
+
+    return render_template('login.html')
+
 
 @app.route("/studentDashboard", methods=['GET'])
 def studentDashboard():
-    return render_template('studentDashboard.html')
+    # Retrieve the studentID from the query parameters
+    student_id = request.args.get('studentID')
 
-@app.route("/form", methods=['GET'])
+    # Pass the studentID to the studentDashboard.html template
+    return render_template('studentDashboard.html', studentID=student_id)
+
+
+def list_files(bucket, path):
+    contents = []
+    folder_prefix = path
+
+    for image in bucket.objects.filter(Prefix=folder_prefix):
+        # Extract file name without the folder prefix
+        file_name = image.key[len(folder_prefix):]
+        if file_name:
+            contents.append(file_name)
+
+    return contents
+
+
+@app.route("/form", methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    if request.method == 'POST':
+        studID = request.form['studentID']
 
-@app.route("/report", methods=['GET'])
+        uploaded_files = request.files.getlist('acceptanceForm') + \
+            request.files.getlist('parentForm') + \
+            request.files.getlist('letterForm') + \
+            request.files.getlist('hireEvi')
+
+        # Uplaod image file in S3 #
+        # emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        # Create a folder or prefix for the files in S3
+        # Replace 'your_folder_name' with your desired folder name
+        folder_name = 'Student/' + studID + "/"
+
+        list_files = []
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+
+            for file in uploaded_files:
+                list_files.append(file.filename)
+                # Construct the key with the folder prefix and file name
+                key = folder_name + file.filename
+
+                # Upload the file into the specified folder
+                s3.Bucket(custombucket).put_object(Key=key, Body=file)
+
+                # Generate the object URL
+                bucket_location = boto3.client(
+                    's3').get_bucket_location(Bucket=custombucket)
+                s3_location = (bucket_location['LocationConstraint'])
+
+                if s3_location is None:
+                    s3_location = ''
+                else:
+                    s3_location = '-' + s3_location
+
+                object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                    s3_location,
+                    custombucket,
+                    file.filename)
+
+        except Exception as e:
+            return str('bucket', str(e))
+
+        bucket = s3.Bucket(custombucket)
+
+        # list_of_files = list_files(bucket)
+
+        return render_template('form.html', my_bucket=bucket, studentID=studID, list_of_files=list_files)
+
+    # Retrieve the studentID from the query parameters
+    student_id = request.args.get('studentID')
+
+    return render_template('form.html', studentID=student_id)
+
+
+@app.route("/report", methods=['GET', 'POST'])
 def report():
-    return render_template('report.html')
+    if request.method == 'POST':
+        studID = request.form['studentID']
+        reportForm_files = request.files['reportForm']
+
+        # Uplaod image file in S3
+        s3 = boto3.resource('s3')
+
+        # Create a folder or prefix for the files in S3
+        folder_name = 'Student/' + studID + "/" + "report/"
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+
+            filename = reportForm_files.filename.split('.')
+
+            # Construct the key with the folder prefix and file name
+            key = folder_name + filename[0] + "_progress_report." + filename[1]
+
+            # Upload the file into the specified folder
+            s3.Bucket(custombucket).put_object(Key=key, Body=reportForm_files)
+
+            # Generate the object URL
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                key)
+
+        except Exception as e:
+            return str('bucket', str(e))
+
+        bucket = s3.Bucket(custombucket)
+        list_of_files = list_files(bucket, folder_name)
+
+        return render_template('report.html', my_bucket=bucket, studentID=studID, list_of_files=list_of_files)
+
+    # Retrieve the studentID from the query parameters
+    studID = request.args.get('studentID')
     
+    folder_name = 'Student/' + studID + "/" + "report/"
+
+    # Uplaod image file in S3
+    s3 = boto3.resource('s3')
+
+    bucket = s3.Bucket(custombucket)
+    list_of_files = list_files(bucket, folder_name)
+
+    return render_template('report.html', my_bucket=bucket, studentID=studID, list_of_files=list_of_files)
+
+# -------------------------------------------------------------- Student End --------------------------------------------------------------#
+
+
+# -------------------------------------------------------------- Lecturer START (Kuah Jia Yu) --------------------------------------------------------------#
+
 @app.route("/lectRegister", methods=['GET', 'POST'])
 def lectRegister():
     if request.method == 'POST':
@@ -132,29 +353,29 @@ def lectRegister():
 
         insert_sql = "INSERT INTO lecturer VALUES (%s, %s, %s, %s, %s)"
         cursor = db_conn.cursor()
-        
+
         try:
-            cursor.execute(insert_sql, (lectName, 
-                                        lectID, 
-                                        lectEmail, 
-                                        gender, 
+            cursor.execute(insert_sql, (lectName,
+                                        lectID,
+                                        lectEmail,
                                         gender,
                                         password
-
                                         ))
             db_conn.commit()
             cursor.close()
-            return redirect(url_for('login'))  # Go to the dashboard after successful registration
+            # Go to the dashboard after successful registration
+            return redirect(url_for('login'))
         except Exception as e:
             cursor.close()
             return str(e)  # Handle any database errors here
     return render_template('lectRegister.html')
-    
+
+
 @app.route("/lectLogin", methods=['GET', 'POST'])
 def lectLogin():
     if request.method == 'POST':
         return render_template('index.html', user_authenticated=True)
-    
+
     # Fetch data from the database here
     cursor = db_conn.cursor()
     select_sql = "SELECT lectEmail, password FROM lecturer"
@@ -162,10 +383,23 @@ def lectLogin():
     data = cursor.fetchall()
     cursor.close()
     return render_template('lectLogin.html', lecturer=data)
-    
+
+
 @app.route("/lectDashboard", methods=['GET'])
 def lectDashboard():
     return render_template('lectDashboard.html')
+
+# ------------------------------------------------------------------- Lecturer END -------------------------------------------------------------------#
+
+# ------------------------------------------------------------------- Company START (Wong Kar Yan) -------------------------------------------------------------------#
+
+@app.route("/companyDashboard", methods=['GET'])
+def companyDashboard():
+    return render_template('companyDashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
 
 @app.route("/compRegister", methods=['GET', 'POST'])
 def compRegister():
@@ -174,6 +408,16 @@ def compRegister():
         compEmail = request.form['compEmail']
         comPassword = request.form['comPassword']
 
+        # Check if the email is already in the database.
+        cursor = db_conn.cursor()
+        cursor.execute("SELECT * FROM company WHERE compEmail=%s", (compEmail))
+        results = cursor.fetchall()
+        cursor.close()
+
+        # If the email is already in the database, return an error message to the user and display it on the register.html page.
+        if len(results) > 0:
+            return render_template('compRegister.html', email_error="This company email is already in use.")
+        
         insert_sql = "INSERT INTO company VALUES (%s, %s, %s)"
         cursor = db_conn.cursor()
         
@@ -184,24 +428,11 @@ def compRegister():
                                         ))
             db_conn.commit()
             cursor.close()
-            return redirect(url_for('compLogin'))  # Go to the dashboard after successful registration
+            return redirect(url_for('login'))  # Go to the dashboard after successful registration
         except Exception as e:
             cursor.close()
             return str(e)  # Handle any database errors here
     return render_template('companyRegister.html')
-
-@app.route("/compLogin", methods=['GET', 'POST'])
-def compLogin():
-    if request.method == 'POST':
-        return render_template('index.html', user_authenticated=True)
-    
-    # Fetch data from the database here
-    cursor = db_conn.cursor()
-    select_sql = "SELECT compEmail, comPassword FROM company"
-    cursor.execute(select_sql)
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('compLogin.html', lecturer=data)
 
 @app.route("/jobReg", methods=['GET', 'POST'])
 def jobReg():
@@ -258,6 +489,8 @@ def jobReg():
 @app.route("/companyDashboard", methods=['GET'])
 def companyDashboard():
     return render_template('companyDashboard.html')
+
+# ------------------------------------------------------------------- Company END -------------------------------------------------------------------#
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
